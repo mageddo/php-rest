307b90c1dc624f57b62cd83f8b142d9390cc07bc
Elvis <edigitalb@gmail.com>
2016-03-04 17:55:05 -0300

    reorganizando o código

--- classes/RetornoJson.php
+++ classes/RetornoJson.php
@@ -1,21 +1,17 @@
 <?php
 class RetornoJson {
-	
-	/**
-	 * 1 = sucesso, 0 = erro
-	 * @var int
-	 */
+
 	public $estado;
 	public $dados; 
 	
 	function __construct($estado, $dados){
-		http_response_code($estado);
-		header('Content-Type: application/json; charset=utf-8');
 		$this->estado = $estado;
 		$this->dados = $dados;
 	}
 	
 	function __toString(){
+		@http_response_code($estado);
+		@header('Content-Type: application/json; charset=utf-8');
 		return json_encode($this); 
 	}
 }
