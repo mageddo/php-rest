1b30035248d9daedcd70369643ed5fb508b7a4cc
Elvis <edigitalb@gmail.com>
2016-04-02 13:06:29 -0300

    fixando status

--- classes/RetornoJson.php
+++ classes/RetornoJson.php
@@ -2,19 +2,32 @@
 class RetornoJson {
 
 	public $estado;
-	public $dados; 
+	public $dados;
+	public $headers;
 	
-	function __construct($estado, $dados){
+	function __construct($estado, $dados, $headers = array('Content-Type: application/json; charset=utf-8')){
 		$this->estado = $estado;
 		$this->dados = $dados;
+		$this->headers = $headers;
 	}
 	
 	function __toString(){
 		@http_response_code($this->estado);
-		@header('Content-Type: application/json; charset=utf-8');
+		@header('Access-Control-Allow-Origin: *');
+		foreach ($this->headers as $header) {
+			@header($header);
+		}
 		if(MG_DEBUG){
 			return json_encode($this->dados, JSON_PRETTY_PRINT + JSON_UNESCAPED_UNICODE);
 		}
 		return json_encode($this->dados); 
+	}
+
+	static function invalidField($message){
+		die(new RetornoJson(Status::$BAD_REQUEST, array('message' => $message)));
+	}
+
+	static function success($o){
+		die(new RetornoJson(Status::$OK, $o));
 	}
 }
--- classes/Status.php
+++ classes/Status.php
@@ -4,5 +4,5 @@ class Status {
 	public static $BAD_REQUEST = 400;
 	public static $UNAUTHORIZED = 401;
 	public static $NOT_FOUND = 404;
-	public static $INTERAL_SERVER_ERROR = 500;
+	public static $INTERNAL_SERVER_ERROR = 500;
 }
